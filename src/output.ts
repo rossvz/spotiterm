c0f5469ceeb7e09619ac07938aa805df87c32643
Ross van Zyl <rossvz@getsellr.com>
2018-10-22 22:57:28 -0400

    :lipstick: (image output) centered album artwork within terminal

--- src/output.ts
+++ src/output.ts
@@ -2,11 +2,18 @@ import terminalImage from 'terminal-image'
 import center from 'center-align'
 import termSize from 'term-size'
 
+const emptyLine = () => new Array(termSize().columns).fill(' ').join('')
+
 export async function printToConsole(data, body: any) {
   const term = termSize()
-  const image = body ? await terminalImage(body, { height: term.rows - 5 }) : ''
+  const image = body
+    ? await terminalImage(body, { width: term.columns / 2 })
+    : ''
+  const pad = new Array(Math.round(term.columns / 4)).fill(' ').join('')
+  console.log(emptyLine())
   console.log(center(`Track: ${data.track}`, term.columns))
   console.log(center(`Artist: ${data.artist}`, term.columns))
   console.log(center(`Album: ${data.album}`, term.columns))
-  console.log(center(image, term.columns))
+  console.log(emptyLine())
+  console.log(`${pad}${image}`)
 }
